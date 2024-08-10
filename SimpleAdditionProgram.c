257820fbd27c1adc6772cb9ab3ccadc7c6d8ef86
SAGAR T <ssagu6661@gmail.com>
2024-08-10 12:58:09 -0400

    Add files via upload

---
+++ SimpleAdditionProgram.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int num1,num2;
+    printf("enter two numbers:\n");
+    scanf("%d%d",& num1,& num2);
+    printf("Addition of two numbers %d\n",num1+num2);
+    printf("subtraction of two numbers %d\n",num1-num2);
+    printf("multiplication of two numbers %d\n",num1*num2);
+    printf("division of two numbers %d\n",num1/num2);
+    return 0;
+    
+}
